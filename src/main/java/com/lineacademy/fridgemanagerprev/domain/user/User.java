543dc485957018fbb30952fe3f2ea80ca3f22dc3
Tae.Garam (Office) <drasla217@gmail.com>
2026-08-12 15:48:11 +0900

    Notice Entity 작성

--- src/main/java/com/lineacademy/fridgemanagerprev/domain/user/User.java
+++ src/main/java/com/lineacademy/fridgemanagerprev/domain/user/User.java
@@ -3,9 +3,7 @@ package com.lineacademy.fridgemanagerprev.domain.user;
 import com.lineacademy.fridgemanagerprev.domain.common.BaseTimeEntity;
 import com.lineacademy.fridgemanagerprev.domain.enums.RoleType;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -39,4 +37,25 @@ public class User extends BaseTimeEntity {
     private RoleType role = RoleType.USER;
 
     // TODO: 다른 테이블과의 관계를 기록해줘야 함
+
+    @Builder
+    private User(String nickname, String password, String email, LocalDateTime birthdate, RoleType role) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.birthdate = birthdate;
+        if (role != null) this.role = role;
+    }
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void updateBirthdate(LocalDateTime birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 }
