71a07bc552842b1863a19d4a2912109955b7ef57
Tae.Garam (Office) <drasla217@gmail.com>
2026-08-13 11:12:43 +0900

    Category 엔티티 작성

--- src/main/java/com/lineacademy/fridgemanagerprev/domain/user/User.java
+++ src/main/java/com/lineacademy/fridgemanagerprev/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.lineacademy.fridgemanagerprev.domain.user;
 
+import com.lineacademy.fridgemanagerprev.domain.category.Category;
 import com.lineacademy.fridgemanagerprev.domain.common.BaseTimeEntity;
 import com.lineacademy.fridgemanagerprev.domain.enums.RoleType;
 import com.lineacademy.fridgemanagerprev.domain.fridge.Fridge;
@@ -45,6 +46,9 @@ public class User extends BaseTimeEntity {
     // cascade는, 연결지은 이 user가 삭제될 경우 연결될 fridge는 어떻게 할까?
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Fridge> fridges = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Category> categories = new ArrayList<>();
 
     @Builder
     private User(String nickname, String password, String email, LocalDateTime birthdate, RoleType role) {
